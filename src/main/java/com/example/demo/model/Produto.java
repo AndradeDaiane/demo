0b7814a33de9a3f9b943d2a48b5b732429675edd
AndradeDaiane <daiane.santosasd23@gmail.com>
2025-04-04 19:30:11 -0300

    desenvolvendo

--- src/main/java/com/example/demo/model/Produto.java
+++ src/main/java/com/example/demo/model/Produto.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType; // Define a estratégia de geração 
 import jakarta.persistence.Id; // Marca o campo como chave primária.
 import lombok.AllArgsConstructor; // Gera automaticamente um construtor com todos os campos.
 import lombok.Data; // Gera automaticamente getters, setters, equals, hashCode e toString.
+import lombok.NoArgsConstructor;
 
 // Configura o escaneamento de entidades no pacote especificado.
 @EntityScan("com.example.demo.model")
@@ -20,6 +21,8 @@ import lombok.Data; // Gera automaticamente getters, setters, equals, hashCode e
 // Gera automaticamente um construtor que aceita todos os campos como parâmetros.
 
 @AllArgsConstructor
+@NoArgsConstructor // Gera automaticamente um construtor sem parâmetros.
+// Define a classe Produto como uma entidade JPA, representando um produto no sistema.
 public class Produto {
 
     // Marca o campo "id" como chave primária da tabela.
